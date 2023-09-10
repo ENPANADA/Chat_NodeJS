77474651348da41b952a3e00a52ddb36c75ff681
ENPANADA <92269511+ENPANADA@users.noreply.github.com>
2023-09-09 18:01:51 -0600

    Update index.js

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ io.on('connection', (socket) => {
     console.log('made socket connection', socket.id);
     // Handle chat event
     socket.on('chat', function(data){
-        // console.log(data);
+        console.log(data);
         io.sockets.emit('chat', data);
     });
     // Handle typing event
